8271760b7904d9018b305bbb2489b33db93c29b1
Weslley Araujo <weslleyaraujo@weslleys-air.lan>
2022-08-10 22:00:56 +0200

    nextjs works?

---
+++ pages/api/hello.ts
@@ -0,0 +1,65 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cheerio from "cheerio";
+const PAGE_URL = "https://penr.stachanov.com/penr/currentAvailability/index";
+
+type Status = "closed" | "no information" | "available";
+const cache = new Map();
+export default async function handler(req, res) {
+  const $ = cheerio.load(
+    await fetch(PAGE_URL, {
+      headers: {
+        "accept-language": "en-GB,en-US",
+      },
+    }).then((response) => response.text())
+  );
+
+  const parse = (content: string) => content.replace(/[\r|\n]/g, "").trim();
+  const toNumber = (value: string) => Number(value.replace(/\./g, ""));
+
+  const [date, time] = parse($("#lastUpdate").text())
+    .split(" ")
+    .filter(Boolean)
+    .filter((item) => !/Last|update\:/.test(item)) as string[];
+
+  const created = new Date(`${date} ${time}`);
+  const createdKey = created.toUTCString();
+
+  if (
+    cache.has(createdKey) &&
+    new Date().getTime() - created.getTime() > 1000 * 60 * 1
+  ) {
+    console.log("from cache");
+    const data = cache.get(createdKey);
+    return data;
+  }
+  const data: {
+    location: string;
+    availability: Status;
+    spaces: number;
+  }[] = $("table tbody tr")
+    .toArray()
+    .map((row) => {
+      const [location, availability, spaces] = $(row)
+        .find("td")
+        .toArray()
+        .map((element) => parse($(element).text()));
+
+      return {
+        location,
+        availability: availability.toLowerCase() as Status,
+        spaces: toNumber(spaces),
+      };
+    });
+
+  console.log("clear and new cache");
+  cache.clear();
+  cache.set(createdKey, {
+    data,
+    update: {
+      date,
+      time,
+    },
+  });
+
+  res.status(200).json(cache.get(createdKey));
+}
