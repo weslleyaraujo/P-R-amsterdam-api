2375973f247e44e1154f306228c946d1eea7ea22
Weslley Araujo <weslleyaraujo20@gmail.com>
2022-09-10 21:31:25 +0200

    tweak cache

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -60,6 +60,6 @@ export default async function handler(req, res) {
     },
   };
 
-  res.setHeader("Cache-Control", "s-maxage=60");
+  res.setHeader("Cache-Control", "max-age=0, s-maxage=60");
   res.status(200).json(response);
 }
